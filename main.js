01e8eb3e2b257bd53762b980b4f1678320e5a8d7
Elza <aez7@ya.ru>
2022-12-18 14:30:44 +0300

    1

---
+++ main.js
@@ -0,0 +1,35 @@
+function sendMessage() {
+        //получаем ввод от пользователя
+        let msg = document.getElementById('msg').value;
+
+        //отправляем запрос и получаем данные
+        let xhr = new XMLHttpRequest();
+        xhr.open('GET','https://nordic.sierghieipielie.repl.co/?messeg=' + msg,false);
+        xhr.send();
+
+        
+        let mess = document.getElementById('msg_input');
+
+        let json = sendRequestGET("https://nordic.sierghieipielie.repl.co/index.php");
+
+        //раскодируем данные
+        let data = JSON.parse(json); 
+        
+        console.log(data);
+
+        for (let i = 0; i < data.length; i++) {
+            //выводим данные шаблона
+            document.getElementById('chat-box__body').innerHTML += document.getElementById('tmpl_mes').innerHTML.replace('${name}', data[i]["name"])
+                                                                                                                .replace('${message}', data[i]['messeg'])
+                                                                                                                .replace('${date}', data[i]["date"]);
+        }
+    }
+
+    function sendRequestGET(url) {
+        let xhr = new XMLHttpRequest();
+        xhr.open('GET', url, false);
+        xhr.send();
+
+        //отдает данные(результат)
+        return xhr.responseText;
+    }
