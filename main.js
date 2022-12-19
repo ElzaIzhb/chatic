baad9017477bea08874bd17549339b430aef40b6
StacyMch <greensummertea@gmail.com>
2022-12-19 20:42:23 +0300

    добавлена стилизация оболочки

--- main.js
+++ main.js
@@ -46,3 +46,21 @@
         //отдает данные(результат)
         return xhr.responseText;
     }
+
+    
+    //Код jquery для растягивания поля сообщения по высоте контента (только для textarea)
+    $(function() {
+        $('#msg').on('input keyup paste', function() {
+          var $el = $(this),
+              offset = $el.innerHeight() - $el.height();
+      
+          if ($el.innerHeight() < this.scrollHeight) {
+            // Grow the field if scroll height is smaller
+            $el.height(this.scrollHeight - offset);
+          } else {
+            // Shrink the field and then re-set it to the scroll height in case it needs to shrink
+            $el.height(1);
+            $el.height(this.scrollHeight - offset);
+          }
+        });
+      });
